868a256b83621fc83eba3b4fc807f4d7401d30b1
kuratus89 <kuratus89@gmail.com>
2026-08-22 03:26:46 +0530

    Implement boot logo display and stage management in main.cpp; add stage and log level enums in main.h

---
+++ include/main.h
@@ -0,0 +1,12 @@
+#pragma once
+enum class Stage{
+    BOOT_STAGE,
+    CHECKUP_STAGE,
+    ERROR_STAGE,
+};
+
+enum class LogLevel{
+    INFO ,
+    WARN ,
+    ERRO , 
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
+#include "../include/main.h"
 #include <Arduino.h>
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <string>
+// #include <string>
+#include <vector>
+
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define LOGO_NAME "KURA"
 
 // PINS
 #define LED_PIN 2
@@ -14,31 +19,103 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH , SCREEN_HEIGHT , &Wire , -1);
 
-int cnt =0;
+#define LOGO_WIDTH 32
+#define LOGO_HEIGHT 32
 
-void setup() {
+//Global variables 
+Stage stage;
+std::vector<String> sysLog;
+
+const unsigned char logo[] PROGMEM = {
+0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00,
+0x00, 0x03, 0xC0, 0x00,
+0x00, 0x0C, 0x20, 0x00,
+0x00, 0x10, 0x1C, 0x00,
+0x00, 0x60, 0x06, 0x00,
+0x01, 0xC0, 0x03, 0x80,
+0x03, 0x00, 0x01, 0xC0,
+0x0F, 0x00, 0x00, 0x70,
+0x1F, 0xC0, 0x01, 0xF8,
+0x11, 0xF0, 0x07, 0x98,
+0x10, 0x7C, 0x1F, 0x18,
+0x1C, 0x1E, 0x78, 0x08,
+0x1F, 0xC7, 0xC0, 0x08,
+0x13, 0xE1, 0x80, 0x18,
+0x10, 0x39, 0x80, 0x38,
+0x10, 0x1D, 0x80, 0xC8,
+0x18, 0x0F, 0x81, 0x88,
+0x18, 0x07, 0x8F, 0x88,
+0x1C, 0x03, 0xFC, 0x88,
+0x17, 0x81, 0xE0, 0xC8,
+0x11, 0xC1, 0x80, 0xC8,
+0x09, 0xFF, 0x80, 0xD8,
+0x05, 0x9F, 0x80, 0x60,
+0x03, 0x87, 0x80, 0xC0,
+0x01, 0xC1, 0x83, 0x80,
+0x00, 0x61, 0x8E, 0x00,
+0x00, 0x19, 0xB8, 0x00,
+0x00, 0x07, 0xC0, 0x00,
+0x00, 0x02, 0x80, 0x00,
+0x00, 0x00, 0x00, 0x00,
+};
+
+
+void boot(){
   Serial.begin(115200);
   pinMode(LED_PIN , OUTPUT);
   digitalWrite(LED_PIN , HIGH);
   Wire.begin(OLED_SDA , OLED_SCL);
+}
+
+
+void displayBootLogo(){
+  display.clearDisplay();
+  display.drawBitmap((SCREEN_WIDTH - LOGO_WIDTH)/2 , (SCREEN_HEIGHT - LOGO_HEIGHT)/2 , logo , 32 , 32 ,SSD1306_WHITE);
+  
+  display.setCursor((SCREEN_WIDTH - (4*6))/2 , ((SCREEN_HEIGHT - LOGO_HEIGHT)/2)+LOGO_HEIGHT + 1 );
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  
+  display.println(LOGO_NAME);
+  display.display();
+}
+
+void pushLog(String value , LogLevel logLevel){
+    
+}
+
+void setup() {
+  boot();
+  stage = Stage::CHECKUP_STAGE;
+
   if(!display.begin(SSD1306_SWITCHCAPVCC , 0x3C)){
     Serial.println("oled not found!");
+    stage = Stage::ERROR_STAGE;
     return;
   }
-  display.clearDisplay();
-  display.setTextSize(5);
-  display.setTextColor(SSD1306_WHITE);
+
+  displayBootLogo();
+  delay(1000);
+}
+
+static inline void errorStage(){
+  digitalWrite(LED_PIN , HIGH);
+  delay(10);
+  digitalWrite(LED_PIN , LOW);
+  delay(15);
+}
+
+static inline void checkUpStage(){
 
 }
 
 void loop() {
-  display.clearDisplay();
-  display.setCursor(0,0);
-  display.println(cnt);
-  cnt++;
-  display.display();
-  String ser = "display number " + String(cnt);
-  Serial.println(ser);
-  // delay(5);
+  switch(stage){
+    case Stage::ERROR_STAGE:errorStage();break;
+    case Stage::CHECKUP_STAGE:checkUpStage();break;
+
+  }
 }
 
